4eb32c434d202c463f54d74c1267bbd9ba195503
y.sergeev <y.sergeev@spectrumit.ru>
2024-10-10 16:21:58 +0400

    init

---
+++ lib/determinant_calculator.ex
@@ -0,0 +1,18 @@
+defmodule DeterminantCalculator do
+  @moduledoc """
+  Documentation for `DeterminantCalculator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DeterminantCalculator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/determinant_calculator_test.exs
@@ -0,0 +1,8 @@
+defmodule DeterminantCalculatorTest do
+  use ExUnit.Case
+  doctest DeterminantCalculator
+
+  test "greets the world" do
+    assert DeterminantCalculator.hello() == :world
+  end
+end
